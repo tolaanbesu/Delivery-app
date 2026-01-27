61f456f2f4bff5768524eef0b21af3243275237a
eyuda <eyudawit5@gmail.com>
2026-01-27 20:48:13 +0300

    3

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const protect = (req, res, next)=>{
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if(!token){
+        res.status(401).json({message:"Not authorized"});
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(401).json({message: "Invalid token"});
+    }
+};
+
+export default protect;
